783d87b02e457d1b8c203eaaaad6f9e3e79395e2
Rage65 <123thetechguy@gmail.com>
2024-09-02 14:29:31 -0700

    adding photos and changed script
    I modiued the python script to change the file extension to .JPEG as for whatever reason githup pages does not like .jpg

--- photogrophy/list images.py
+++ photogrophy/list images.py
@@ -1,22 +1,38 @@
 import os
 import json
 
-def get_image_files(directory):
+def change_extensions_and_collect(directory):
     # List of image extensions to look for
     image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'}
     
     # List to store image filenames
     image_files = []
-    
+    changed_files = []
+
     # Iterate over files in the specified directory
     for filename in os.listdir(directory):
         full_path = os.path.join(directory, filename)
         if os.path.isfile(full_path):
             # Check file extension
             if any(filename.lower().endswith(ext) for ext in image_extensions):
+                # Collect original filenames
                 image_files.append(filename)
-    
-    return image_files
+                
+                # If file extension is .jpg, change to .JPEG
+                if filename.lower().endswith('.jpg'):
+                    new_filename = filename[:-4] + '.JPEG'
+                    new_full_path = os.path.join(directory, new_filename)
+                    
+                    # Rename the file
+                    os.rename(full_path, new_full_path)
+                    
+                    # Collect the new filename
+                    changed_files.append(new_filename)
+                else:
+                    # Collect filenames that were not changed
+                    changed_files.append(filename)
+
+    return image_files, changed_files
 
 def write_json(data, filename):
     with open(filename, 'w') as json_file:
@@ -29,13 +45,15 @@ def main():
     # Path to the JSON file
     json_file_path = r'C:\Users\Daniel\Documents\GitHub\rage65.github.io\photogrophy\images_list.json'
     
-    # Get list of image files
-    image_files = get_image_files(images_directory)
+    # Change file extensions and get list of image files
+    original_files, updated_files = change_extensions_and_collect(images_directory)
     
-    # Write the list to a JSON file
-    write_json(image_files, json_file_path)
+    # Write the list of updated filenames to a JSON file
+    write_json(updated_files, json_file_path)
     
-    print(f'Updated {json_file_path} with {len(image_files)} image filenames.')
+    print(f'Updated {json_file_path} with {len(updated_files)} image filenames.')
+    print(f'Original filenames: {original_files}')
+    print(f'Renamed files: {updated_files}')
 
 if __name__ == "__main__":
     main()
