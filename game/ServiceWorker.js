c08c86b74a8a353aad0ad743cb0353e407c72da7
Rage65 <123thetechguy@gmail.com>
2024-09-26 17:08:27 -0700

    upadated quality for less gpu load

--- game/ServiceWorker.js
+++ game/ServiceWorker.js
@@ -1,8 +1,8 @@
 const cacheName = "Rage65-2d platformer-1.0";
 const contentToCache = [
-    "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
+    "Build/868ad3e5f43e7a2d53d450d58e45443c.loader.js",
     "Build/d1842b4dd2fced1a1aa8139b91e1dd25.framework.js",
-    "Build/40b4254f451e51e8ddb444de6e99b418.data",
+    "Build/43770093618b44a49600e601def25864.data",
     "Build/f0df29fce9b78b022dbc82cef47ac138.wasm",
     "TemplateData/style.css"
 
